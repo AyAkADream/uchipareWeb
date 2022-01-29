dcbcb4cf6b3c10140f72cca634dbcbceace4009f
AyakaSakura <g18951ta@gm.tsuda.ac.jp>
2022-01-29 16:49:41 +0900

    .

---
+++ public/js/config.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAEob3wykM5zglVTuhdIbj3jYUCdbfQELc",
+  authDomain: "fir-test4pro2.firebaseapp.com",
+  databaseURL: "https://fir-test4pro2-default-rtdb.firebaseio.com",
+  projectId: "fir-test4pro2",
+  storageBucket: "fir-test4pro2.appspot.com",
+  messagingSenderId: "363820866715",
+  appId: "1:363820866715:web:1703ebd781873b506aba3e",
+  measurementId: "G-89BZXWYHW4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ public/myblock.js
@@ -0,0 +1,325 @@
+Blockly.defineBlocksWithJsonArray(
+  /* ここにJSONファイルの内容をコピー */
+  [{
+    "type": "dance1_l",
+    "message0": "dance1_l",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance1_lを踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance1_r",
+    "message0": "dance1_r",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance1_rを踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance2_l",
+    "message0": "dance2_l",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance2_lを踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance2_r",
+    "message0": "dance2_r",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance2_rを踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance3_1",
+    "message0": "dance3_1",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance3_1を踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance3_2",
+    "message0": "dance3_2",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance3_2を踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance4_1",
+    "message0": "dance4_1",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance4_1を踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance4_2",
+    "message0": "dance4_2",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance4_2を踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance5",
+    "message0": "dance5",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance5を踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance6_l",
+    "message0": "dance6_l",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance6_lを踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance6_r",
+    "message0": "dance6_r",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance6_rを踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance7_l",
+    "message0": "dance7_l",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance7_lを踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance7_r",
+    "message0": "dance7_r",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance7_rを踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance8_1_l",
+    "message0": "dance8_1_l",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance8_1_lを踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance8_1_r",
+    "message0": "dance8_1_r",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance8_1_rを踊ります",
+    "helpUrl": ""
+  },
+  {
+    "type": "dance8_2",
+    "message0": "dance8_2",
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "#000000",
+    "tooltip": "dance8_2を踊ります",
+    "helpUrl": ""
+  }]
+);
+
+Blockly.JavaScript['dance1_l'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance1_l();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance1_r'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance1_r();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance2_l'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance2_l();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance2_r'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance2_r();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance3_1'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance3_1();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance3_2'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance3_2();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance4_1'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance4_1();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance4_2'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance4_2();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance5'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance5();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance6_l'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance6_l();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance6_r'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance6_r();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance7_l'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance7_l();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance7_r'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance7_r();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance8_1_l'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance8_1_l();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance8_1_r'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance8_1_r();\n';
+  return code;
+};
+
+Blockly.JavaScript['dance8_2'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'dance8_2();\n';
+  return code;
+};
+
+var array1 = new Array();
+
+function dance1_l(){
+  console.log("dance1_l");
+  array1.push("Dance1_L");
+}
+
+function dance1_r(){
+  console.log("dance1_r");
+  array1.push("Dance1_R");
+}
+
+function dance2_l(){
+  console.log("dance2_l");
+  array1.push("Dance2_L");
+}
+
+function dance2_r(){
+  console.log("dance2_r");
+  array1.push("Dance2_R");
+}
+
+function dance3_1(){
+  console.log("dance3_1");
+  array1.push("Dance3_1");
+}
+
+function dance3_2(){
+  console.log("dance3_2");
+  array1.push("Dance3_2");
+}
+
+function dance4_1(){
+  console.log("dance4_1");
+  array1.push("Dance4_1");
+}
+
+function dance4_2(){
+  console.log("dance4_2");
+  array1.push("Dance4_2");
+}
+
+function dance5(){
+  console.log("dance5");
+  array1.push("Dance5");
+}
+
+function dance6_l(){
+  console.log("dance6_l");
+  array1.push("Dance6_L");
+}
+
+function dance6_r(){
+  console.log("dance6_r");
+  array1.push("Dance6_R");
+}
+
+function dance7_l(){
+  console.log("dance7_l");
+  array1.push("Dance7_L");
+}
+
+function dance7_r(){
+  console.log("dance7_r");
+  array1.push("Dance7_R");
+}
+
+function dance8_1_l(){
+  console.log("dance8_1_l");
+  array1.push("Dance8_1_L");
+}
+
+function dance8_1_r(){
+  console.log("dance8_1_r");
+  array1.push("Dance8_1_R");
+}
+
+function dance8_2(){
+  console.log("dance8_2");
+  array1.push("Dance8_2");
+}
